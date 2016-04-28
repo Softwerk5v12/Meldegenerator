94907ca9399f23f4aaa34b79076f86eea9f79509
Alexander Köppelmayr <alex.koeppelm@gmail.com>
2016-04-28 11:09:00 +0200

    Geänderte Oberfläche

---
+++ Meldegenerator/Meldegenerator/MainWindow.xaml.vb
@@ -0,0 +1,13 @@
+﻿Class MainWindow
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+
+        'Dim BT As New Button
+
+        'BT.Width = 400
+        'BT.Height = 200
+
+
+        'WPMain.Children.Add(BT)
+
+    End Sub
+End Class
