48b436b7876e8daf6d76f89ea02d1e2e2b6617bc
FREIZEIT <Freizeitsw_solution@gmail.com>
2016-04-28 13:12:12 +0200

    Progress Bar

---
+++ Meldegenerator/Meldegenerator/ProgressBar.xaml.vb
@@ -0,0 +1,23 @@
+﻿Public Class ProgressBar
+
+
+    Public Sub DoWork()
+
+        MsgBox("fefeiojfioejiofj")
+    End Sub
+
+    Private Sub Rectangle_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs)
+
+
+
+    End Sub
+
+
+    Public Function OpenProjekt(ByVal Pfad As String) As Boolean
+
+
+        PGBarDaten.Value = 0
+
+
+    End Function
+End Class
