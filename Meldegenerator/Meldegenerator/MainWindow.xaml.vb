e4bf0b81bf55c7f1ad4749ce2c158a9882b01f3c
FREIZEIT <Freizeitsw_solution@gmail.com>
2016-04-28 13:17:24 +0200

    ProgreessBar and File Open

--- Meldegenerator/Meldegenerator/MainWindow.xaml.vb
+++ Meldegenerator/Meldegenerator/MainWindow.xaml.vb
@@ -1,17 +1,40 @@
-﻿Class MainWindow
+﻿Imports System.Windows.Threading
+Imports System.IO
+Imports System.Windows.Forms
+
+Class MainWindow
+
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
 
-        Dim BT As New Button
-
-        BT.Width = 400
-        BT.Height = 200
-
-
-        WPMain.Children.Add(BT)
 
 
 
-        Dim Fred As String = "Fred"
+
+
+        '  Dispatcher.BeginInvoke(Sub() PGbar.DoWork(), DispatcherPriority.Background)                                 )
+
+    End Sub
+
+
+
+
+    Private Sub TiaProjektauswählen()
+        ' Dim OR As open
+
+
+        'PGbar.OpenProjekt()
+
+
+    End Sub
+
+    Private Sub ProjektÖffnen_Click(sender As Object, e As RoutedEventArgs)
+        Dim OFD As New OpenFileDialog With {.Multiselect = False, .Filter = "TIA files (*.ap13)|*.ap13"}
+
+        OFD.ShowDialog()
+
+
+        MsgBox(OFD.FileName)
 
     End Sub
 End Class
+
