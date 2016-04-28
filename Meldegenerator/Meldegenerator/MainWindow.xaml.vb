68e19da0ccaaa9b2ed542e3b7cc4d703c459615d
FREIZEIT <Freizeitsw_solution@gmail.com>
2016-04-28 11:22:29 +0200

    StartProjekt

--- Meldegenerator/Meldegenerator/MainWindow.xaml.vb
+++ Meldegenerator/Meldegenerator/MainWindow.xaml.vb
@@ -1,13 +1,16 @@
 ﻿Class MainWindow
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
 
-        'Dim BT As New Button
+        Dim BT As New Button
 
-        'BT.Width = 400
-        'BT.Height = 200
+        BT.Width = 400
+        BT.Height = 200
 
 
-        'WPMain.Children.Add(BT)
+        WPMain.Children.Add(BT)
 
+
+
+        Dim Fred As String = "fefeff"
     End Sub
 End Class
